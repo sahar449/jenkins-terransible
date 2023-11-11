e9200bfaa006b7c0945ed174f50ac84c2c1727cc
sahar449 <saharr449@gmail.com>
2023-11-11 13:15:09 +0200

    first

--- version.tf
+++ version.tf
@@ -1,10 +1,15 @@
-# Terraform Block
-terraform {
-  required_version = "~> 1.6" 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+# # Terraform Block
+# terraform {
+#   required_version = "~> 1.6" 
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
+
+# Provider Block
+provider "aws" {
+  region = "us-east-1"
 }
