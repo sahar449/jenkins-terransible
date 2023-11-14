d4cbbcbb280ea11a5b2195a7fc761907152e1a2b
sahar449 <saharr449@gmail.com>
2023-11-14 16:14:40 +0200

    change permissions

--- main.tf
+++ main.tf
@@ -24,3 +24,12 @@ terraform {
     region = "us-east-1"
   }
 }
+
+output "prometheus" {
+  value = join(":", [aws_instance.prometheus.public_ip, 9090])
+}
+
+output "grafana" {
+  value = join(":", [aws_instance.grafana.public_ip, 3000])
+}
+
