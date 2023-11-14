2b6405384a55fb93b90519aac39f869bcdd2d9c7
sahar449 <saharr449@gmail.com>
2023-11-14 16:08:02 +0200

    change permissions

--- main.tf
+++ main.tf
@@ -1,16 +1,7 @@
 module "Monitoring" {
   source = "./tf_module"
-  #profile = "default"
   access_key = ""
   secret_key = "" 
-}
-
-terraform {
-  backend "s3" {
-    bucket = "sahar-tf"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
 }
 
 #Provider Block
@@ -18,7 +9,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# # Terraform Block
+#Terraform Block
 terraform {
   required_version = "~> 1.6" 
   required_providers {
@@ -26,5 +17,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+  backend "s3" {
+    bucket = "sahar-tf"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
   }
 }
