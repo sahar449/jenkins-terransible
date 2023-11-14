d4fdd35ea1613f1b9710114ad250d8746916f15d
sahar449 <saharr449@gmail.com>
2023-11-14 16:01:32 +0200

    added profile

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 module "Monitoring" {
   source = "./tf_module"
   profile = "default"
-  region = ""
   access_key = ""
   secret_key = "" 
 }
@@ -11,5 +10,22 @@ terraform {
     bucket = "sahar-tf"
     key    = "terraform.tfstate"
     region = "us-east-1"
+  }
+}
+
+#Provider Block
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+}
+
+# # Terraform Block
+terraform {
+  required_version = "~> 1.6" 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
