de9ac86242610d1671fbc0c47f3c25a9850cb594
sahar449 <saharr449@gmail.com>
2023-11-14 16:17:47 +0200

    change permissions

--- tf_module/version.tf
+++ tf_module/version.tf
@@ -1,16 +1,16 @@
-# Terraform Block
-terraform {
-  required_version = "~> 1.6" 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-#Provider Block
-provider "aws" {
-  region = "us-east-1"
-  profile = "default"
-}
+# # Terraform Block
+# terraform {
+#   required_version = "~> 1.6" 
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
+# #Provider Block
+# provider "aws" {
+#   region = "us-east-1"
+#   profile = "default"
+# }
 
