987f36dc57df424e2c81422a41252c6d60df30b6
sahar449 <saharr449@gmail.com>
2024-03-17 18:20:42 +0200

    ,

--- tf_module/data.tf
+++ tf_module/data.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "latest_ubuntu" {
     most_recent = true
-    owners = [ "amazon" ]
+    owners = [ "aws-marketplace" ]
     filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-*"]
